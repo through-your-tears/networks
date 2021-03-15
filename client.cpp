b8b1fafaa81c46fad1ca42ed61982d36625bd0dc
through-your-tears <62816146+through-your-tears@users.noreply.github.com>
2021-03-16 01:01:18 +0300

    Update client.cpp

--- client.cpp
+++ client.cpp
@@ -4,21 +4,42 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <Windows.h>
 
 using namespace std;
 
 void main() {
-	const unsigned short int id = 1;
+	const unsigned short int id = 0;
 	string s;
+	int prev = 0;
 	while (1) {
+		int k = 0;
+		ifstream f("C:/Users/Dmitriy/source/repos/server0/server0/output0.txt");
+		
+		while (getline(f, s))
+		{
+			k++;
+		}
+		f.close();
+		if (k > prev) {
+			ifstream h("C:/Users/Dmitriy/source/repos/server0/server0/output0.txt");
+			while (getline(h, s))
+			{
+				cout << s << "\n";
+			}
+			h.close();
+		}
+		
 		ofstream g("message.txt", ios_base::app);
-		cout << "lastname, weight, height";
+		cout << "lastname, weight, height" << "\n";
 		getline(cin, s);
 		while (s != " ") {
 			g << s << "\n";
 			getline(cin, s);
 		}
 		g.close();
+		prev = k;
+		Sleep(2000);
 	}
 }
 
